343ac8e6384f1b54105aedbd88c4a79501d540e6
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-24 17:28:50 -0800

    Redirect home page to login page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
-router.get('/', (req, res, next) => {
-  res.render('layout', {
-    view: 'index',
+router.get('/', showHomePage);
+router.get('/login', showLogin);
+
+function authenticate(req, res, next, callback) {
+  User.findById(req.session.userId, (error, user) => {
+    if (error) {
+      return next(error);
+    } else {
+      if (user === null) {
+        res.redirect('login');
+      } else {
+        callback(user);
+      }
+    }
   });
-});
+}
+
+function showHomePage(req, res, next) {
+  authenticate(req, res, next, (user) => {
+    res.render('layout', {
+      view: 'index',
+      user: user,
+    });
+  });
+};
+
+function showLogin(req, res, next) {
+  res.render('layout', {
+    view: 'login',
+    user: null,
+  });
+};
 
 module.exports = router;
