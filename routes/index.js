d0de9f4ffeb62cc931f0a7db1addf2d9fb02e2cc
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-24 20:47:20 -0800

    Move user and login-related routes to user router

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,6 @@ const habitatModels = require('../gameModels/habitats.js');
 const dragonModels = require('../gameModels/dragons.js');
 
 router.get('/', showHomePage);
-router.get('/login', showLogin);
-router.post('/login', loginUser);
-router.post('/signup', signupUser);
-router.get('/logout', logoutUser);
 router.post('/buyHabitat/:habitatIndex', buyHabitat);
 router.post('/buyDragon/:dragonIndex/incubate', buyDragon);
 router.post('/sellHabitat/:habitatId', sellHabitat);
@@ -56,77 +52,6 @@ function showHomePage(req, res, next) {
     });
   });
 };
-
-function showLogin(req, res, next) {
-  res.render('layout', {
-    view: 'login',
-    currentUser: null,
-  });
-};
-
-function loginUser(req, res, next) {
-  let username = req.body.loginUsername;
-  let password = req.body.loginPassword;
-
-  if (username && password) {
-    User.authenticate(username, password, (error, user) => {
-      if (error || !user) {
-        var err = new Error('Wrong username or password.');
-        err.status = 401;
-        return next(err);
-      } else {
-        req.session.userId = user._id;
-        return res.redirect('/');
-      }
-    });
-  } else {
-    var err = new Error('All fields are required.');
-    err.status = 400;
-    return next(err);
-  }
-}
-
-function signupUser(req, res, next) {
-  var userData = {
-    username: req.body.username,
-    password: req.body.password,
-    passwordConf: req.body.passwordConf,
-  };
-
-  if (userData.password !== userData.passwordConf) {
-    var err = new Error('Passwords do not match.');
-    err.status = 400;
-    res.send('Passwords do not match.');
-    return next(err);
-  }
-
-  if (req.body.username && req.body.password && req.body.passwordConf) {
-    User.create(userData, function (error, user) {
-      if (error) {
-        return next(error);
-      } else {
-        req.session.userId = user._id;
-        return res.redirect('/');
-      }
-    });
-  } else {
-    var err = new Error('All fields are required.');
-    err.status = 400;
-    return next(err);
-  }
-}
-
-function logoutUser(req, res, next) {
-  if (req.session) {
-    req.session.destroy(function (err) {
-      if (err) {
-        return next(err);
-      } else {
-        return res.redirect('/');
-      }
-    });
-  }
-}
 
 function buyHabitat(req, res, next) {
   authenticate(req, res, next, (user) => {
