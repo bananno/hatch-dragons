92162af18e0e39eb4c59368e176d406b88aabfd7
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-24 18:19:29 -0800

    Remove unhashed password confirmation from model

--- models/user.js
+++ models/user.js
@@ -12,10 +12,6 @@ var UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  passwordConf: {
-    type: String,
-    required: true,
-  }
 });
 
 // authenticate input against database
