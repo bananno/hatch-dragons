4b020048361c6446fb9bf62f520d1e3899ed0457
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-24 20:01:47 -0800

    Add habitats to list

--- gameModels/habitats.js
+++ gameModels/habitats.js
@@ -3,6 +3,42 @@ const habitats = [
     name: 'Plant',
     image: 'https://vignette.wikia.nocookie.net/dragonvale/images/5/55/PlantHabitat2013.png',
   },
+  {
+    name: 'Fire',
+    image: 'https://vignette.wikia.nocookie.net/dragonvale/images/d/dc/FireHabitat2013.png',
+  },
+  {
+    name: 'Earth',
+    image: 'https://vignette.wikia.nocookie.net/dragonvale/images/9/96/EarthHabitat2013.png',
+  },
+  {
+    name: 'Cold',
+    image: 'https://vignette.wikia.nocookie.net/dragonvale/images/4/42/ColdHabitat2013.png',
+  },
+  {
+    name: 'Lightning',
+    image: 'https://vignette.wikia.nocookie.net/dragonvale/images/6/69/LightningHabitat2013.png',
+  },
+  {
+    name: 'Water',
+    image: 'https://vignette.wikia.nocookie.net/dragonvale/images/0/08/WaterHabitat2013.png',
+  },
+  {
+    name: 'Air',
+    image: 'https://vignette.wikia.nocookie.net/dragonvale/images/7/7c/AirHabitat2013.png',
+  },
+  {
+    name: 'Metal',
+    image: 'https://vignette.wikia.nocookie.net/dragonvale/images/0/08/WaterHabitat2013.png',
+  },
+  {
+    name: 'Light',
+    image: 'https://vignette.wikia.nocookie.net/dragonvale/images/3/38/MetalHabitat2013.png',
+  },
+  {
+    name: 'Dark',
+    image: 'https://vignette.wikia.nocookie.net/dragonvale/images/c/c6/DarkHabitat.png',
+  },
 ];
 
 module.exports = habitats;
