0d03eba4fbe28de2f2ff158545306d060960507e
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-25 09:29:22 -0800

    Add missing body parser code

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const usersRouter = require('./routes/users');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(session({
   secret: 'work hard',
   resave: true,
