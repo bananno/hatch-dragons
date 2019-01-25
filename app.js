cc50d062b8e773ecfcfb54cddd2bd53d0b21b256
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-24 17:15:58 -0800

    Add packages and database connection to app

--- app.js
+++ app.js
@@ -3,13 +3,31 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
+mongoose.connect('mongodb://localhost/hatchDragons');
+var db = mongoose.connection;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
