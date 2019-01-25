c39cc7e928609f71ce108cebaae286574e1cbeaf
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-24 17:49:38 -0800

    Create login and signup routes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var User = require('../models/user');
 
 router.get('/', showHomePage);
 router.get('/login', showLogin);
+router.post('/login', loginUser);
+router.post('/signup', signupUser);
 
 function authenticate(req, res, next, callback) {
   User.findById(req.session.userId, (error, user) => {
@@ -34,5 +36,98 @@ function showLogin(req, res, next) {
     user: null,
   });
 };
+
+function loginUser(req, res, next) {
+  let username = req.body.loginUsername;
+  let password = req.body.loginPassword;
+
+  if (username && password) {
+    User.authenticate(username, password, (error, user) => {
+      if (error || !user) {
+        var err = new Error('Wrong email or password.');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/');
+      }
+    });
+  } else {
+    var err = new Error('All fields are required.');
+    err.status = 400;
+    return next(err);
+  }
+}
+
+function signupUser(req, res, next) {
+  var userData = {
+    username: req.body.username,
+    password: req.body.password,
+    passwordConf: req.body.passwordConf,
+  };
+
+  if (userData.password !== userData.passwordConf) {
+    var err = new Error('Passwords do not match.');
+    err.status = 400;
+    res.send('Passwords do not match.');
+    return next(err);
+  }
+
+  if (req.body.username && req.body.password && req.body.passwordConf) {
+    User.create(userData, function (error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/');
+      }
+    });
+  } else {
+    var err = new Error('All fields are required.');
+    err.status = 400;
+    return next(err);
+  }
+}
+
+function old(req, res, next) {
+
+  if (req.body.email &&
+    req.body.username &&
+    req.body.password &&
+    req.body.passwordConf) {
+
+    var userData = {
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+      passwordConf: req.body.passwordConf,
+    }
+
+    User.create(userData, function (error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+
+  } else if (req.body.logemail && req.body.logpassword) {
+    User.authenticate(req.body.logemail, req.body.logpassword, function (error, user) {
+      if (error || !user) {
+        var err = new Error('Wrong email or password.');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  } else {
+    var err = new Error('All fields required.');
+    err.status = 400;
+    return next(err);
+  }
+}
 
 module.exports = router;
