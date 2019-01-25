fa156868cfb355b397c1cd4757219e4a5f1926e3
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-25 09:11:57 -0800

    Begin adding a sample post request

--- app.js
+++ app.js
@@ -23,6 +23,23 @@ app.use(session({
   })
 }));
 
+
+
+app.get('/api/hello', (req, res) => {
+  console.log('TESTING ONE');
+  res.send({ express: 'Hello From Express' });
+});
+
+app.post('/api/world', (req, res) => {
+  console.log('TESTING TWO');
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  state = {
+    response: '',
+    post: '',
+    responseToPost: '',
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch('/api/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+    const body = await response.text();
+
+    this.setState({ responseToPost: body });
+  };
+
   render() {
     return (
       <div className="App">
-        HatchDragons
+        <h1>HatchDragons</h1>
+
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <strong>Post to Server:</strong>
+          </p>
+          <input
+            type="text"
+            value={this.state.post}
+            onChange={e => this.setState({ post: e.target.value })}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <p>{this.state.responseToPost}</p>
+
       </div>
     );
   }
