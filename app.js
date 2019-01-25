9fd106379ef7f06fe91f3f268f0298a0f98a4e4e
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-25 09:16:03 -0800

    Specify port in server file

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const port = process.env.PORT || 5000;
 
 app.use(session({
   secret: 'work hard',
@@ -71,5 +72,8 @@ app.use(function(err, req, res, next) {
     currentUser: null,
   });
 });
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 module.exports = app;
