7850f39211c2d415b96aafcde72c236808b98445
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-24 18:43:51 -0800

    Create habitat model

---
+++ models/habitat.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const HabitatSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  }
+});
+
+const Habitat = mongoose.model('Habitat', HabitatSchema);
+module.exports = Habitat;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
+var Habitat = require('../models/habitat');
 
 router.get('/', showHomePage);
 router.get('/login', showLogin);
