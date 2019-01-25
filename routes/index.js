7bb54c40687eb7f5f98ec4f07027b214ab049fbf
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-24 17:59:53 -0800

    Delete extra function

--- routes/index.js
+++ routes/index.js
@@ -90,47 +90,6 @@ function signupUser(req, res, next) {
   }
 }
 
-function old(req, res, next) {
-
-  if (req.body.email &&
-    req.body.username &&
-    req.body.password &&
-    req.body.passwordConf) {
-
-    var userData = {
-      email: req.body.email,
-      username: req.body.username,
-      password: req.body.password,
-      passwordConf: req.body.passwordConf,
-    }
-
-    User.create(userData, function (error, user) {
-      if (error) {
-        return next(error);
-      } else {
-        req.session.userId = user._id;
-        return res.redirect('/profile');
-      }
-    });
-
-  } else if (req.body.logemail && req.body.logpassword) {
-    User.authenticate(req.body.logemail, req.body.logpassword, function (error, user) {
-      if (error || !user) {
-        var err = new Error('Wrong email or password.');
-        err.status = 401;
-        return next(err);
-      } else {
-        req.session.userId = user._id;
-        return res.redirect('/profile');
-      }
-    });
-  } else {
-    var err = new Error('All fields required.');
-    err.status = 400;
-    return next(err);
-  }
-}
-
 function logoutUser(req, res, next) {
   if (req.session) {
     req.session.destroy(function (err) {
