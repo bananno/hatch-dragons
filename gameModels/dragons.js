850e9639747792da8f5be49b39d848f41a8cf1d1
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-24 20:39:53 -0800

    Add dragon egg images and use for incubator stage

--- gameModels/dragons.js
+++ gameModels/dragons.js
@@ -1,21 +1,24 @@
 const dragons = [
   {
     name: 'Plant',
-    image: {
-      baby: 'https://vignette.wikia.nocookie.net/dragonvale/images/b/bf/PlantDragonBaby.png',
-    },
+    images: [
+      'https://vignette.wikia.nocookie.net/dragonvale/images/7/7b/PlantDragonEgg.png',
+      'https://vignette.wikia.nocookie.net/dragonvale/images/b/bf/PlantDragonBaby.png',
+    ],
   },
   {
     name: 'Fire',
-    image: {
-      baby: 'https://vignette.wikia.nocookie.net/dragonvale/images/5/54/FireDragonBaby.png',
-    },
+    images: [
+      'https://vignette.wikia.nocookie.net/dragonvale/images/f/f5/FireDragonEgg.png',
+      'https://vignette.wikia.nocookie.net/dragonvale/images/5/54/FireDragonBaby.png',
+    ],
   },
   {
     name: 'Earth',
-    image: {
-      baby: 'https://vignette.wikia.nocookie.net/dragonvale/images/5/5c/EarthDragonBaby.png',
-    },
+    images: [
+      'https://vignette.wikia.nocookie.net/dragonvale/images/e/e2/EarthDragonEgg.png',
+      'https://vignette.wikia.nocookie.net/dragonvale/images/5/5c/EarthDragonBaby.png',
+    ],
   },
 ];
 
