6eecc1af582251c93c8b0b785ab5cf4896201187
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-24 17:43:14 -0800

    Clean up spacing

--- models/user.js
+++ models/user.js
@@ -24,10 +24,10 @@ var UserSchema = new mongoose.Schema({
   }
 });
 
-//authenticate input against database
-UserSchema.statics.authenticate = function (email, password, callback) {
+// authenticate input against database
+UserSchema.statics.authenticate = function(email, password, callback) {
   User.findOne({ email: email })
-    .exec(function (err, user) {
+    .exec(function(err, user) {
       if (err) {
         return callback(err)
       } else if (!user) {
@@ -35,7 +35,7 @@ UserSchema.statics.authenticate = function (email, password, callback) {
         err.status = 401;
         return callback(err);
       }
-      bcrypt.compare(password, user.password, function (err, result) {
+      bcrypt.compare(password, user.password, function(err, result) {
         if (result === true) {
           return callback(null, user);
         } else {
@@ -45,10 +45,10 @@ UserSchema.statics.authenticate = function (email, password, callback) {
     });
 }
 
-//hashing a password before saving it to the database
-UserSchema.pre('save', function (next) {
+// hash password before saving it to the database
+UserSchema.pre('save', function(next) {
   var user = this;
-  bcrypt.hash(user.password, 10, function (err, hash) {
+  bcrypt.hash(user.password, 10, function(err, hash) {
     if (err) {
       return next(err);
     }
@@ -57,7 +57,5 @@ UserSchema.pre('save', function (next) {
   })
 });
 
-
 var User = mongoose.model('User', UserSchema);
 module.exports = User;
-
