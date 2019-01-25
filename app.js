c06bc5a23b3fd81be9451c7c19685f83583b00aa
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-24 20:46:00 -0800

    Clean up users router

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
