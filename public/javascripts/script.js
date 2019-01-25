2181d8dbc70a24d598a12bd6efdd348a563cc7e2
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-24 21:45:16 -0800

    Add JS and CSS to expand the habitats to show more details

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,14 @@
 
-console.log('running');
-
 function hatchEgg(dragonId) {
   console.log(dragonId);
   $('.place-hatched-egg').filter('[dragon_id="' + dragonId + '"]').show();
+}
+
+function expandHabitat(habitatId) {
+  unExpandHabitats();
+  $('.habitat[habitat_id="' + habitatId + '"]').removeClass('basic').addClass('expanded');
+}
+
+function unExpandHabitats() {
+  $('.habitat').removeClass('expanded').addClass('basic');
 }
