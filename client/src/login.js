063a375597717142868aefad26d63683140d9caf
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-25 09:52:39 -0800

    Begin building requests for signup and login forms

--- client/src/login.js
+++ client/src/login.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  state = {
+    post: '',
+    loginPostResponse: '',
+  };
+
+  handleSubmitLogin = async e => {
+    e.preventDefault();
+    const response = await fetch('/api/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+    const body = await response.text();
+
+    this.setState({ loginPostResponse: body });
+  };
+
+  handleSubmitSignup = async e => {
+    e.preventDefault();
+    const response = await fetch('/api/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+    const body = await response.text();
+
+    this.setState({ loginPostResponse: body });
+  };
+
   render () {
     return (
       <div>
         <div>
           <h2>Login</h2>
-          <form action="/login" method="post">
+          <form onSubmit={this.handleSubmitLogin}>
               <input type="text" name="loginUsername" placeholder="username" required=""/>
               <input type="password" name="loginPassword" placeholder="password" required=""/>
               <button type="submit">LOGIN</button>
@@ -14,13 +47,14 @@ class Login extends Component {
         </div>
         <div>
           <h2>Signup</h2>
-          <form action="/signup" method="post">
+          <form onSubmit={this.handleSubmitSignup}>
               <input type="text" name="username" placeholder="username" required=""/>
               <input type="password" name="password" placeholder="password" required=""/>
               <input type="password" name="passwordConf" placeholder="confirm password" required=""/>
               <button type="submit">REGISTER</button>
           </form>
         </div>
+        <p style={{border:"1px solid black"}}>{'(' + this.state.loginPostResponse + ')'}</p>
       </div>
     );
   }
