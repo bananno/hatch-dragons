74e681114f26a07b9b7972cba704571ae8901709
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-25 17:03:22 -0800

    Show habitats and dragons in the store

--- client/src/Store/Store.js
+++ client/src/Store/Store.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import habitatModels from '../gameModels/habitats';
+import dragonModels from '../gameModels/dragons';
 
 class Store extends Component {
   render () {
@@ -7,8 +9,32 @@ class Store extends Component {
     }
 
     return (
-      <div style={{border: "1px solid black"}}>
+      <div style={{border: "3px solid black"}}>
         <h2>STORE</h2>
+        <div style={{border: "1px solid black"}}>
+          <h3>Habitats</h3>
+          {
+            habitatModels.map((model, i) => {
+              return (
+                <div key={i}>
+                  {model.name} Habitat
+                </div>
+              );
+            })
+          }
+        </div>
+        <div style={{border: "1px solid black"}}>
+          <h3>Dragons</h3>
+          {
+            dragonModels.map((model, i) => {
+              return (
+                <div key={i}>
+                  {model.name} Dragon
+                </div>
+              );
+            })
+          }
+        </div>
       </div>
     );
   }
