f99a771d5c6a6beb6725f9578fc7a9b309c0682e
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-28 11:33:04 -0800

    Limit results in each section to 4 per page

--- client/src/Market/MarketSection.js
+++ client/src/Market/MarketSection.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
 
+const resultsPerPage = 4;
+
 class MarketSection extends Component {
+  state = {
+    pageNumber: 0
+  }
+
   render () {
+    let startIndex = this.state.pageNumber;
+    let modelsOnPage = this.props.models.slice(startIndex, startIndex + resultsPerPage);
+    let showBackButton = startIndex > 0;
+    let showNextButton = startIndex + resultsPerPage < this.props.models.length;
+
     return (
       <div className="market-section">
+        {
+          showBackButton ? <button>BACK</button> : null
+        }
+        {
+          modelsOnPage.map((model, i) => {
+            return (
+              <div key={i} className="market-item">
+                <h4>{model.name}</h4>
+                <button onClick={this.props.onPurchase(i)}>buy</button>
+              </div>
+            );
+          })
+        }
+        {
+          showNextButton ? <button>NEXT</button> : null
+        }
       </div>
     );
   }
