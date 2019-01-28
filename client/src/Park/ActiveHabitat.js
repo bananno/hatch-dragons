443eac451df64a043cf096da557d054cacb66ef3
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-27 19:46:04 -0800

    Begin adding content to active habitat

--- client/src/Park/ActiveHabitat.js
+++ client/src/Park/ActiveHabitat.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import Modal from '../containers/modal';
+import habitatModels from '../gameModels/habitats';
 
 class Habitat extends Component {
   render () {
-    if (this.props.rootState.activeHabitat == null) {
+    let habitat = this.props.rootState.activeHabitat;
+
+    if (habitat == null) {
       return null;
     }
 
+    let className = 'habitat active';
+
+    let gameModel = habitatModels.filter(model => {
+      return model.name === habitat.gameModel;
+    })[0];
+
+    console.log(habitat);
+
     return (
-      <Modal>
-        HABITAT
+      <Modal className={className}>
+        <div>
+          <img src={gameModel.image} className="habitat-image" alt=""/>
+        </div>
       </Modal>
     );
   }
