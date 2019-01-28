9d3b66224c3fc2afb96f06bbaeca378a2d5f02eb
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-28 12:39:10 -0800

    Add images to market

--- client/src/Market/marketItem.js
+++ client/src/Market/marketItem.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const marketItem = (props) => {
+  let imageSrc = props.model.image || props.model.images[1];
+
   return (
     <div className="market-item">
       <h4>{props.model.name}</h4>
+      <div className="image-frame">
+        <img src={imageSrc} alt=""/>
+      </div>
       <button onClick={props.onPurchase}>buy</button>
     </div>
   );
