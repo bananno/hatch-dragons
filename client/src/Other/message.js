a2973bb8be710d8a018171868821170b5c65d2b4
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-28 13:13:43 -0800

    Show message when hatching dragon

--- client/src/Other/message.js
+++ client/src/Other/message.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const message = (props) => {
+  let message = null;
+
+  if (props.rootState.hatchDragon) {
+    message = 'Select a habitat for the dragon.';
+  }
+
+  if (message == null) {
+    return null;
+  }
+
   return (
-    <div>
-      Message here...
+    <div className="message">
+      {message}
     </div>
   );
 };
