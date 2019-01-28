ae7d86895287041732f67c010b7a6bed7af9ad1d
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-27 21:55:52 -0800

    Begin adding separate component for sections in market

--- client/src/Market/Market.js
+++ client/src/Market/Market.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Modal from '../containers/modal';
+import MarketSection from './MarketSection';
 import habitatModels from '../gameModels/habitats';
 import dragonModels from '../gameModels/dragons';
 
@@ -44,6 +45,13 @@ class Market extends Component {
         <div className="market">
           <h2>MARKET</h2>
           <h3>Habitats</h3>
+          <MarketSection models={habitatModels} onPurchase={this.buyHabitat}/>
+          <h3>Dragons</h3>
+          <MarketSection models={dragonModels} onPurchase={this.buyDragon}/>
+
+          <br/>
+          <br/>
+
           <div className="market-section">
             {
               habitatModels.map((model, i) => {
---
+++ client/src/Market/MarketSection.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class MarketSection extends Component {
+  render () {
+    return (
+      <div className="market-section">
+      </div>
+    );
+  }
+}
+
+export default MarketSection;
