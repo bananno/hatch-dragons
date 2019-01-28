eb61b6a12fa9d87c98a3eb116e792e4264c15001
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-28 13:08:08 -0800

    Remove reference to 'public' folder

--- server.js
+++ server.js
@@ -35,7 +35,6 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/', usersRouter);
