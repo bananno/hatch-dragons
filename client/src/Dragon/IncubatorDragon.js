065352b406941ff4213b9bb3505fbea7c06b78af
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-29 10:06:14 -0800

    Move functions and variables to be class attributes

--- client/src/Dragon/IncubatorDragon.js
+++ client/src/Dragon/IncubatorDragon.js
@@ -2,34 +2,36 @@ import React, { Component } from 'react';
 import findModel from '../gameModels/findModel';
 
 class IncubatorDragon extends Component {
+  state = {}
+
+  gameModel = findModel('dragon', this.props.dragon);
+
+  onHatch = () => {
+    this.props.setRootState({
+      placeDragon: this.props.dragon,
+      showIncubator: false
+    });
+  }
+
   render() {
-    let gameModel = findModel('dragon', this.props.dragon);
-
     let className = 'dragon egg';
-
-    const onHatch = () => {
-      this.props.setRootState({
-        placeDragon: this.props.dragon,
-        showIncubator: false
-      });
-    };
 
     let now = new Date().getTime();
 
     let secondsElapsed = Math.round((now - this.props.dragon.timestamp)/1000);
 
-    let secondsNeeded = gameModel.eggTime[2] + gameModel.eggTime[1] * 60
-      + gameModel.eggTime[0] * 60 * 60;
+    let secondsNeeded = this.gameModel.eggTime[2] + this.gameModel.eggTime[1] * 60
+      + this.gameModel.eggTime[0] * 60 * 60;
 
     let hatching = secondsElapsed >= secondsNeeded;
 
     return (
       <div className={className}>
-        <h1>{gameModel.name} Dragon</h1>
-        <img src={gameModel.images[0]} alt=""/><br/>
+        <h1>{this.gameModel.name} Dragon</h1>
+        <img src={this.gameModel.images[0]} alt=""/><br/>
         {
           hatching
-          ? <button onClick={onHatch}>hatch</button>
+          ? <button onClick={this.onHatch}>hatch</button>
           : 'waiting... (' + (secondsNeeded - secondsElapsed) + ' seconds remaining)'
         }
       </div>
