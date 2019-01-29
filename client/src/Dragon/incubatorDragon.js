6c5b676bb74f563edcb0a1786077f55cf297fac1
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-29 09:40:05 -0800

    Begin calculating the relative date in UTC

--- client/src/Dragon/incubatorDragon.js
+++ client/src/Dragon/incubatorDragon.js
@@ -20,14 +20,39 @@ const incubatorDragon = (props) => {
     return {
       year: parseInt(part1[0]),
       month: parseInt(part1[1]),
-      day: parseInt(part1[2]),
+      date: parseInt(part1[2]),
       hour: parseInt(part2[0]),
       minute: parseInt(part2[1]),
       second: parseInt(part2[2])
     };
   }
 
+  console.log(props.dragon.timestamp)
+
   let then = parseTimestamp(props.dragon.timestamp);
+
+  console.log(then);
+
+  let now = new Date();
+
+  console.log('year');
+  console.log(then.year);
+  console.log(now.getUTCFullYear());
+  console.log('month');
+  console.log(then.month);
+  console.log(now.getUTCMonth() + 1);
+  console.log('date');
+  console.log(then.date);
+  console.log(now.getUTCDate());
+  console.log('hour');
+  console.log(then.hour);
+  console.log(now.getUTCHours());
+  console.log('minute');
+  console.log(then.minute);
+  console.log(now.getUTCMinutes() + 1);
+  console.log('second');
+  console.log(then.second);
+  console.log(now.getUTCSeconds() + 1);
 
   return (
     <div className={className}>
