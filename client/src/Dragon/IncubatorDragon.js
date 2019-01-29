f58be0a2c8db6adc7c7045b18d340fb6882b8cd3
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-29 10:00:41 -0800

    Update component to full class version

--- client/src/Dragon/IncubatorDragon.js
+++ client/src/Dragon/IncubatorDragon.js
@@ -1,38 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import findModel from '../gameModels/findModel';
 
-const IncubatorDragon = (props) => {
-  let gameModel = findModel('dragon', props.dragon);
+class IncubatorDragon extends Component {
+  render() {
+    let gameModel = findModel('dragon', this.props.dragon);
 
-  let className = 'dragon egg';
+    let className = 'dragon egg';
 
-  const onHatch = () => {
-    props.setRootState({
-      placeDragon: props.dragon,
-      showIncubator: false
-    });
-  };
+    const onHatch = () => {
+      this.props.setRootState({
+        placeDragon: this.props.dragon,
+        showIncubator: false
+      });
+    };
 
-  let now = new Date().getTime();
+    let now = new Date().getTime();
 
-  let secondsElapsed = Math.round((now - props.dragon.timestamp)/1000);
+    let secondsElapsed = Math.round((now - this.props.dragon.timestamp)/1000);
 
-  let secondsNeeded = gameModel.eggTime[2] + gameModel.eggTime[1] * 60
-    + gameModel.eggTime[0] * 60 * 60;
+    let secondsNeeded = gameModel.eggTime[2] + gameModel.eggTime[1] * 60
+      + gameModel.eggTime[0] * 60 * 60;
 
-  let hatching = secondsElapsed >= secondsNeeded;
+    let hatching = secondsElapsed >= secondsNeeded;
 
-  return (
-    <div className={className}>
-      <h1>{gameModel.name} Dragon</h1>
-      <img src={gameModel.images[0]} alt=""/><br/>
-      {
-        hatching
-        ? <button onClick={onHatch}>hatch</button>
-        : 'waiting... (' + (secondsNeeded - secondsElapsed) + ' seconds remaining)'
-      }
-    </div>
-  );
-};
+    return (
+      <div className={className}>
+        <h1>{gameModel.name} Dragon</h1>
+        <img src={gameModel.images[0]} alt=""/><br/>
+        {
+          hatching
+          ? <button onClick={onHatch}>hatch</button>
+          : 'waiting... (' + (secondsNeeded - secondsElapsed) + ' seconds remaining)'
+        }
+      </div>
+    );
+  }
+}
 
 export default IncubatorDragon;
