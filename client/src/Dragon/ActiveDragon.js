ee2f33c9b52d79756ada6efd418f1d4435b3e5a8
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-28 18:11:47 -0800

    Fix typo

--- client/src/Dragon/ActiveDragon.js
+++ client/src/Dragon/ActiveDragon.js
@@ -24,7 +24,7 @@ class ActiveDragon extends Component {
 
   move = () => {
     this.props.setRootState({
-      moveDragon: this.props.activeDragon,
+      moveDragon: this.props.rootState.activeDragon,
       activeHabitat: null,
       activeDragon: null
     });
