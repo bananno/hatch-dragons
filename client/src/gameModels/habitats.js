34308e2af98201c3269be770d1bc3c085c7cf90a
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-29 11:42:36 -0800

    Add build time for all habitats

--- client/src/gameModels/habitats.js
+++ client/src/gameModels/habitats.js
@@ -3,51 +3,61 @@ const habitats = [
     name: 'Plant',
     elements: ['plant'],
     image: 'https://vignette.wikia.nocookie.net/dragonvale/images/5/55/PlantHabitat2013.png',
+    buildTime: [0, 0, 0],
   },
   {
     name: 'Fire',
     elements: ['fire'],
     image: 'https://vignette.wikia.nocookie.net/dragonvale/images/d/dc/FireHabitat2013.png',
+    buildTime: [0, 5, 0],
   },
   {
     name: 'Earth',
     elements: ['earth'],
     image: 'https://vignette.wikia.nocookie.net/dragonvale/images/9/96/EarthHabitat2013.png',
+    buildTime: [3, 0, 0],
   },
   {
     name: 'Cold',
     elements: ['cold'],
     image: 'https://vignette.wikia.nocookie.net/dragonvale/images/4/42/ColdHabitat2013.png',
+    buildTime: [12, 0, 0],
   },
   {
     name: 'Lightning',
     elements: ['lightning'],
     image: 'https://vignette.wikia.nocookie.net/dragonvale/images/6/69/LightningHabitat2013.png',
+    buildTime: [0, 30, 0],
   },
   {
     name: 'Water',
     elements: ['water'],
     image: 'https://vignette.wikia.nocookie.net/dragonvale/images/0/08/WaterHabitat2013.png',
+    buildTime: [0, 4, 0],
   },
   {
     name: 'Air',
     elements: ['air'],
     image: 'https://vignette.wikia.nocookie.net/dragonvale/images/7/7c/AirHabitat2013.png',
+    buildTime: [8, 0, 0],
   },
   {
     name: 'Metal',
     elements: ['metal'],
     image: 'https://vignette.wikia.nocookie.net/dragonvale/images/3/38/MetalHabitat2013.png',
+    buildTime: [10, 0, 0],
   },
   {
     name: 'Light',
     elements: ['light'],
     image: 'https://vignette.wikia.nocookie.net/dragonvale/images/a/ab/LightHabitat.png',
+    buildTime: [16, 0, 0],
   },
   {
     name: 'Dark',
     elements: ['dark'],
     image: 'https://vignette.wikia.nocookie.net/dragonvale/images/c/c6/DarkHabitat.png',
+    buildTime: [18, 0, 0],
   },
 ];
 
