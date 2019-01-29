795183c1ae58ffd91fe9ce031d8e1a821f27d447
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-29 09:57:06 -0800

    Calculate seconds remaining; show hatch button when ready

--- client/src/Dragon/incubatorDragon.js
+++ client/src/Dragon/incubatorDragon.js
@@ -13,55 +13,24 @@ const incubatorDragon = (props) => {
     });
   };
 
-  const parseTimestamp = (str) => {
-    let part1 = str.slice(0, str.indexOf('T')).split('-');
-    let part2 = str.slice(str.indexOf('T') + 1, str.indexOf('.')).split(':');
+  let now = new Date().getTime();
 
-    return {
-      year: parseInt(part1[0]),
-      month: parseInt(part1[1]),
-      date: parseInt(part1[2]),
-      hour: parseInt(part2[0]),
-      minute: parseInt(part2[1]),
-      second: parseInt(part2[2])
-    };
-  }
+  let secondsElapsed = Math.round((now - props.dragon.timestamp)/1000);
 
-  console.log(props.dragon.timestamp)
+  let secondsNeeded = gameModel.eggTime[2] + gameModel.eggTime[1] * 60
+    + gameModel.eggTime[0] * 60 * 60;
 
-  let then = parseTimestamp(props.dragon.timestamp);
-
-  console.log(then);
-
-  let now = new Date();
-
-  console.log('year');
-  console.log(then.year);
-  console.log(now.getUTCFullYear());
-  console.log('month');
-  console.log(then.month);
-  console.log(now.getUTCMonth() + 1);
-  console.log('date');
-  console.log(then.date);
-  console.log(now.getUTCDate());
-  console.log('hour');
-  console.log(then.hour);
-  console.log(now.getUTCHours());
-  console.log('minute');
-  console.log(then.minute);
-  console.log(now.getUTCMinutes() + 1);
-  console.log('second');
-  console.log(then.second);
-  console.log(now.getUTCSeconds() + 1);
+  let hatching = secondsElapsed >= secondsNeeded;
 
   return (
     <div className={className}>
       <h1>{gameModel.name} Dragon</h1>
       <img src={gameModel.images[0]} alt=""/><br/>
-      <button onClick={onHatch}>hatch</button>
-
-      <br/><br/>
-      <b>started:</b> {props.dragon.timestamp}
+      {
+        hatching
+        ? <button onClick={onHatch}>hatch</button>
+        : 'waiting... (' + (secondsNeeded - secondsElapsed) + ' seconds remaining)'
+      }
     </div>
   );
 };
