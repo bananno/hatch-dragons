9db4771e896e9ef49f26c0fa68c97be3ca62e036
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-28 20:22:50 -0800

    Add list of elements

---
+++ client/src/gameModels/elements.js
@@ -0,0 +1,44 @@
+const elements = [
+  {
+    name: 'Plant',
+    image: 'https://vignette.wikia.nocookie.net/dragonvale/images/1/11/Icon_Plant.png',
+  },
+  {
+    name: 'Fire',
+    image: 'https://vignette.wikia.nocookie.net/dragonvale/images/0/0a/Icon_Fire.png',
+  },
+  {
+    name: 'Earth',
+    image: 'https://vignette.wikia.nocookie.net/dragonvale/images/8/80/Icon_Earth.png',
+  },
+  {
+    name: 'Cold',
+    image: null,
+  },
+  {
+    name: 'Lightning',
+    image: null,
+  },
+  {
+    name: 'Water',
+    image: null,
+  },
+  {
+    name: 'Air',
+    image: null,
+  },
+  {
+    name: 'Metal',
+    image: null,
+  },
+  {
+    name: 'Light',
+    image: null,
+  },
+  {
+    name: 'Dark',
+    image: null,
+  },
+];
+
+module.exports = elements;
