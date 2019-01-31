2d4807bd101bb4f551945702e156c4444656064d
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-31 14:15:51 -0800

    Update main park formatting and style

--- client/src/Park/Park.js
+++ client/src/Park/Park.js
@@ -3,24 +3,18 @@ import Island from './Island';
 
 class Park extends Component {
   render () {
-    if (this.props.rootState.currentUser == null) {
-      return null;
-    }
-
     return (
-      <div>
-        <div>
-          {
-            this.props.rootState.islands.map(island => {
-              return (
-                <Island key={island._id} island={island}
-                  rootState={this.props.rootState}
-                  setRootState={this.props.setRootState}
-                  makePostRequest={this.props.makePostRequest}/>
-              );
-            })
-          }
-        </div>
+      <div className="main-park">
+        {
+          this.props.rootState.islands.map(island => {
+            return (
+              <Island key={island._id} island={island}
+                rootState={this.props.rootState}
+                setRootState={this.props.setRootState}
+                makePostRequest={this.props.makePostRequest}/>
+            );
+          })
+        }
       </div>
     );
   }
