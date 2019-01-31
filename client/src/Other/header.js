8f291d2a5d6773a3cee30236e8d20225cc3edeed
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-30 21:18:44 -0800

    Disable header buttons while purchasing incubator

--- client/src/Other/header.js
+++ client/src/Other/header.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const header = (props) => {
   const currentlyLoggedIn = props.rootState.currentUser != null;
-  const currentlyPlacingDragon = props.rootState.placeDragon != null;
-  const currentlyMakingPurchase = props.rootState.buyHabitat != null;
+
+  const disableButtons = props.rootState.placeDragon != null
+    || props.rootState.buyHabitat != null || props.rootState.buyIncubator;
 
   const openMarket = () => {
     props.setRootState({
@@ -20,10 +21,8 @@ const header = (props) => {
       return null;
     }
 
-    let disabled = currentlyPlacingDragon || currentlyMakingPurchase;
-
     return (
-      <button onClick={openMarket} disabled={disabled}>MARKET</button>
+      <button onClick={openMarket} disabled={disableButtons}>MARKET</button>
     );
   }
 
@@ -57,10 +56,8 @@ const header = (props) => {
       return null;
     }
 
-    let disabled = currentlyPlacingDragon || currentlyMakingPurchase;
-
     return (
-      <button onClick={clickLogout} disabled={disabled}>logout</button>
+      <button onClick={clickLogout} disabled={disableButtons}>logout</button>
     );
   }
 
