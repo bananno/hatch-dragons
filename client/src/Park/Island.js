1683787a39517ca29541e4ae39f803138a5de66b
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-30 18:23:39 -0800

    Add click event for island to make post request to buy habitat

--- client/src/Park/Island.js
+++ client/src/Park/Island.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 
 class Island extends Component {
+  onClick = () => {
+    if (this.props.rootState.buyHabitat != null) {
+      this.buyHabitat();
+    }
+  }
+
+  buyHabitat = () => {
+    this.props.makePostRequest('/buyHabitat', {
+      habitatIndex: this.props.rootState.buyHabitat,
+      island: this.props.island._id,
+    }, {
+      buyHabitat: null
+    });
+  }
+
   render () {
     return (
-      <div className="island">
+      <div className="island" onClick={this.onClick}>
         ISLAND
       </div>
     );
