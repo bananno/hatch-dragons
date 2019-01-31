f55e2e1856c83bd119929fc90916544a1103eb8f
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-30 17:03:36 -0800

    Add a component for islands and display them in the park

---
+++ client/src/Park/Island.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Island extends Component {
+  render () {
+    return (
+      <div className="island">
+        ISLAND
+      </div>
+    );
+  }
+}
+
+export default Island;
--- client/src/Park/Park.js
+++ client/src/Park/Park.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Island from './Island';
 import ParkIncubator from './parkIncubator';
 import ParkHabitat from '../Habitat/ParkHabitat';
 
@@ -11,6 +12,18 @@ class Park extends Component {
     return (
       <div>
         <ParkIncubator rootState={this.props.rootState} setRootState={this.props.setRootState}/>
+        <div>
+          {
+            this.props.rootState.islands.map(island => {
+              return (
+                <Island key={island._id} island={island}
+                  rootState={this.props.rootState}
+                  setRootState={this.props.setRootState}
+                  makePostRequest={this.props.makePostRequest}/>
+              );
+            })
+          }
+        </div>
         <div>
           {
             this.props.rootState.habitats.map(habitat => {
